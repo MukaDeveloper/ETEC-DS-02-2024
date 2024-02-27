7bc6bd426132d32967b887f86fe536fb503aa6ac
MukaDeveloper <muka.bertoni@gmail.com>
2024-02-27 19:55:57 -0300

    Alterações de estrutura

--- Program.cs
+++ Program.cs
@@ -1,98 +1,101 @@
 ﻿using System;
 
-class Program
-{
-    static Boolean on = true;
+namespace Samuel {
 
-    static Boolean DetalharData()
+    class Program
     {
-        Console.Write("Digite uma data no formato DD/MM/AAAA \n");
-        string dataStr = Console.ReadLine();
+        static Boolean on = true;
 
-        if (DateTime.TryParseExact(dataStr, "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime data))
+        static Boolean DetalharData()
         {
-            string diaDaSemana = data.ToString("dddd");
-            string nomeMes = data.ToString("MMMM");
-            Console.WriteLine($"/============================");
-            Console.WriteLine($"| Dia da semana: {diaDaSemana}");
-            Console.WriteLine($"| Mês: {nomeMes}.");
-            Console.WriteLine($"\\============================");
+            Console.Write("Digite uma data no formato DD/MM/AAAA \n");
+            string dataStr = Console.ReadLine();
 
-            // if (data.DayOfWeek == DayOfWeek.Sunday)
-            // {
-                string horaAtual = DateTime.Now.ToString("HH:mm");
-                Console.WriteLine($"Hora atual: {horaAtual}\n");
-            // }
-        }
-        else
-        {
-            Console.WriteLine("Data inválida. Por favor, digite novamente.");
-            return true;
-        }
-        return false;
-    }
-
-    static Boolean CalcularDescontoINSS()
-    {
-        Console.Write("Digite o valor do salário: ");
-        double salario;
-        if (!double.TryParse(Console.ReadLine(), out salario))
-        {
-            Console.WriteLine("Valor inválido. Por favor, digite novamente.");
-            return true;
-        }
-
-        double inss = 0;
-
-        if (salario <= 1212)
-        {
-            inss = salario * 0.075;
-        }
-        else if (salario <= 2427.35)
-        {
-            inss = salario * 0.09;
-        }
-        else if (salario <= 3641.03)
-        {
-            inss = salario * 0.12;
-        }
-        else
-        {
-            inss = salario * 0.14;
-        }
-
-        double salarioDescontado = salario - inss;
-        Console.WriteLine($"Valor do INSS a ser pago: R$ {inss:F2}");
-        Console.WriteLine($"Salário líquido após desconto do INSS: R$ {salarioDescontado:F2}\n");
-        return false;
-    }
-
-    static void Main(string[] args)
-    {
-
-        while(on) {
-            Console.WriteLine("\nEscolha uma opção:");
-            Console.WriteLine("1. Detalhar Data");
-            Console.WriteLine("2. Calcular Desconto INSS");
-            Console.WriteLine("3. Sair\n");
-
-            string opcao = Console.ReadLine();
-
-            switch (opcao)
+            if (DateTime.TryParseExact(dataStr, "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime data))
             {
-                case "1":
-                    on = DetalharData();
-                    break;
-                case "2":
-                    on = CalcularDescontoINSS();
-                    break;
-                case "3":
-                    Console.WriteLine("Saindo do programa.");
-                    on = false;
-                    break;
-                default:
-                    Console.WriteLine("Opção inválida. Tente novamente.");
-                    break;
+                string diaDaSemana = data.ToString("dddd");
+                string nomeMes = data.ToString("MMMM");
+                Console.WriteLine($"/============================");
+                Console.WriteLine($"| Dia da semana: {diaDaSemana}");
+                Console.WriteLine($"| Mês: {nomeMes}.");
+                Console.WriteLine($"\\============================");
+
+                if (data.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    string horaAtual = DateTime.Now.ToString("HH:mm");
+                    Console.WriteLine($"Hora atual: {horaAtual}\n");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Data inválida. Por favor, digite novamente.");
+                return true;
+            }   
+            return false;
+        }
+
+        static Boolean CalcularDescontoINSS()
+        {
+            Console.Write("Digite o valor do salário: ");
+            double salario;
+            if (!double.TryParse(Console.ReadLine(), out salario))
+            {
+                Console.WriteLine("Valor inválido. Por favor, digite novamente.");
+                return true;
+            }
+
+            double inss = 0;
+
+            if (salario <= 1212)
+            {
+                inss = salario * 0.075;
+            }
+            else if (salario <= 2427.35)
+            {
+                inss = salario * 0.09;
+            }
+            else if (salario <= 3641.03)
+            {
+                inss = salario * 0.12;
+            }
+            else
+            {
+                inss = salario * 0.14;
+            }
+
+            double salarioDescontado = salario - inss;
+            Console.WriteLine($"Valor do INSS a ser pago: R$ {inss:F2}");
+            Console.WriteLine($"Salário líquido após desconto do INSS: R$ {salarioDescontado:F2}\n");
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+
+            while(on) {
+                Console.WriteLine("\nEscolha uma opção:");
+                Console.WriteLine("1. Detalhar Data");
+                Console.WriteLine("2. Calcular Desconto INSS");
+                Console.WriteLine("3. Sair\n");
+
+                string escolha = Console.ReadLine();
+
+                switch (escolha)
+                {
+                    case "1":
+                        on = DetalharData();
+                        break;
+                    case "2":
+                        on = CalcularDescontoINSS();
+                        break;
+                    case "3":
+                        Console.WriteLine("Saindo do programa.");
+                        on = false;
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida. Tente novamente.");
+                        break;
+                }
             }
         }
     }
--- obj/Debug/net8.0/Dotnet.AssemblyInfo.cs
+++ obj/Debug/net8.0/Dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d315f0f0d4cec32c66bd28d530f4612198cbe4dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
